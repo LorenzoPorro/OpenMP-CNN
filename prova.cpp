2101e3f385489a4aec3c6f63aa59ed8a6c30a9db
Matteo Pozzi <Matteo Pozzi>
2016-10-13 14:05:46 +0200

    pooling and relu

--- prova.cpp
+++ prova.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+
 /*  1. filter dimension in max-pooling
     2. passing random dimension matrix to convolution function
     3. determine the number of neurons -> size of hidden layers
@@ -13,16 +14,11 @@ using namespace std;
 
 /* convolution function
    input: input matrix, kernel matrix, output matrix, size of input, kernel and output matrices and the bias */
-void convolution(int input[224][224], float kernel[11][11], float output[55][55], int bias){
+void convolution(int** input, float** kernel, float** output, int isize, int ksize, int osize, int bias){
     
-
-    // determine the size of input, kernel and output rows and columns
-    int isize=sizeof(input[0])/sizeof(int);
-    int ksize=sizeof(kernel[0])/sizeof(int);
-    int osize=sizeof(output[0])/sizeof(int);
     
     int stride=(isize - (int)(isize/ksize) * ksize);
-    int iy,ix;  // input y and x
+    int iy,ix;  // input y and x coordinates
 
     for(int oy=0;oy<osize;oy++){
 
@@ -48,13 +44,24 @@ void convolution(int input[224][224], float kernel[11][11], float output[55][55]
 
 
 
+/* ReLU non-linear function: apply the ReLU nonlinear function f(x)=max(0,x) on each input value
+   input: matrix on which applying ReLU function*/
+void relu(float** relu, int size){
+
+    for(int i=0;i<size;i++){
+
+        for(int j=0;j<size;j++){
+
+            relu[i][j]=max((float)0, relu[i][j]);
+        }
+    }
+}
+
+
+
 /* overlapping max-pooling funtion: extract the maximum value from the output of the convolutional operation
    input: input matrix, output matrix, the size of both them, the size of the pooling matrix and the stride */
-void maxpooling(int input[28][28], int output[14][14], int p, int stride){    // overlapping if: stride < p
-    
-    // determine the size of input and output rows and columns
-    int isize=sizeof(input[0])/sizeof(int);
-    int osize=sizeof(output[0])/sizeof(int);
+void maxpooling(int** input, int** output, int isize, int osize, int p, int stride){    // overlapping if: stride < p
     
     for(int oy=0;oy<osize;oy++){
 
@@ -64,7 +71,7 @@ void maxpooling(int input[28][28], int output[14][14], int p, int stride){    //
 
                 for(int fx=ox*stride;fx<p*stride;fx++){
 
-                    output[oy][ox]=max(output[oy][ox], input[fy][fx]);
+                    output[oy][ox] = max(output[oy][ox], input[fy][fx]);
                 }
             }
         }
@@ -74,25 +81,28 @@ void maxpooling(int input[28][28], int output[14][14], int p, int stride){    //
 
 
 
+
+
 int main() {
-    
+
+
     // input images divided in 3 layers
-    int input1[224][224];
-    int input2[224][224];
-    int input3[224][224];
+    int **input1;
+    int **input2;
+    int **input3;
 
-    // 3 kernels which will convolved on the 3 image matrices
-    float kernel1[11][11];
-    float kernel2[11][11];
-    float kernel3[11][11];
-    // bias
-    int bias = 1;
-
-    // output of convolutional operation between input and kernel
-    float conv1[55][55];
-    float conv2[55][55];
-    float conv3[55][55];
-
+    input1=new int *[224];
+    for(int i=0;i<224;i++){
+        input1[i]=new int [224];
+    }
+    input2=new int *[224];
+    for(int i=0;i<224;i++){
+        input2[i]=new int [224];
+    }
+    input3=new int *[224];
+    for(int i=0;i<224;i++){
+        input3[i]=new int [224];
+    }
 
     // random bit values for each image layer
     for(int i=0;i<224;i++){
@@ -103,15 +113,53 @@ int main() {
         }
     }
 
+
+    // 3 kernels which will convolved on the 3 image matrices
+    float **kernel1;
+    float **kernel2;
+    float **kernel3;
+    
+    kernel1=new float *[11];
+    for(int i=0;i<11;i++){
+        kernel1[i]=new float [11];
+    }
+    kernel2=new float *[11];
+    for(int i=0;i<11;i++){
+        kernel2[i]=new float [11];
+    }
+    kernel3=new float *[11];
+    for(int i=0;i<11;i++){
+        kernel3[i]=new float [11];
+    }
+
     // random values between 0 and 1 for each kernel
     for(int i=0;i<11;i++){
         for(int j=0;j<11;j++){
-            kernel1[i][j]=rand()%2;
-            kernel2[i][j]=rand()%2;
-            kernel3[i][j]=rand()%2;
+            kernel1[i][j]=rand()%5 -2;
+            kernel2[i][j]=rand()%5 -2;
+            kernel3[i][j]=rand()%5 -2;
         }
     }
 
+
+    // output of convolutional operation between input and kernel
+    float **conv1;
+    float **conv2;
+    float **conv3;
+
+    conv1=new float *[55];
+    for(int i=0;i<55;i++){
+        conv1[i]=new float [55];
+    }
+    conv2=new float *[55];
+    for(int i=0;i<55;i++){
+        conv2[i]=new float [55];
+    }
+    conv3=new float *[55];
+    for(int i=0;i<55;i++){
+        conv3[i]=new float [55];
+    }
+    
     // zero values in conv output matrix
     for(int i=0;i<55;i++){
         for(int j=0;j<55;j++){
@@ -122,8 +170,13 @@ int main() {
     }
 
 
-    // convolution between input image layers matrices and kernels
-    convolution(input1,kernel1,conv1,bias);
-    convolution(input2,kernel2,conv2,bias);
-    convolution(input3,kernel3,conv3,bias);
+    // convolution between input image layers and kernels
+    convolution(input1,kernel1,conv1,224,11,55,1);
+    convolution(input2,kernel2,conv2,224,11,55,1);
+    convolution(input3,kernel3,conv3,224,11,55,1);
+    
+    // ReLU nonlinearity
+    relu(conv1,55);
+    relu(conv2,55);
+    relu(conv3,55);
 }
