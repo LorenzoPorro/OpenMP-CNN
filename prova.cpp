f0694caed62212895045a48427e1b0f68810f40a
Matteo Pozzi <Matteo Pozzi>
2016-10-15 12:51:17 +0200

    add number of fetures

--- prova.cpp
+++ prova.cpp
@@ -6,36 +6,59 @@
 
 using namespace std;
 
-
+// THE NUMBER OF FEATURES IS 1
 
 
 
 /* convolution function
-   input: input matrix, kernel matrix, output matrix, size of input, kernel and output matrices and the bias */
-void convolution(int*** input, float*** kernel, float*** output, int isize, int ksize, int osize, int bias){
+   input: input matrix, kernel matrix, output matrix, size of input, kernel and output matrices, the bias and the number fo features */
+void convolution(int*** input, float**** kernel, float*** output, int isize, int ksize, int osize, int bias, int feat){
     
     int stride=(isize - (int)(isize/ksize) * ksize);
     int iy,ix;  // input y and x coordinates
+    float acc[3][osize][osize];  // output of the convolution between each layer of input and each layer of kernel (55x55x3)
 
+    // initialization of the accumulator acc
     for(int i=0;i<3;i++){
+        for(int j=0;j<osize;j++){
+            for(int k=0;k<osize;k++){
+                acc[i][j][k]=0;
+            }
+        }
+    }
 
-        for(int oy=0;oy<osize;oy++){
+    for(int f=0;f<feat;f++){
 
-            iy=oy*stride;
-            for(int ox=0;ox<osize;ox++){
-            
-                ix=ox*stride;
-                for(int ky=0;ky<ksize;ky++){
+        for(int i=0;i<3;i++){
 
-                    for(int kx=0;kx<ksize;kx++){
+            for(int oy=0;oy<osize;oy++){
 
-                        output[i][oy][ox] += input[i][iy][ix] * kernel[i][ky][kx] + bias;
-                        ix++;
-                    }
-                    ix=ox*stride;
-                    iy++;
-                }
                 iy=oy*stride;
+                for(int ox=0;ox<osize;ox++){
+            
+                    ix=ox*stride;
+                    for(int ky=0;ky<ksize;ky++){
+
+                        for(int kx=0;kx<ksize;kx++){
+
+                            acc[i][oy][ox] += input[i][iy][ix] * kernel[f][i][ky][kx] + bias;
+                            ix++;
+                        }
+                        ix=ox*stride;
+                        iy++;
+                    }
+                    iy=oy*stride;
+                }
+            }
+        }
+    }
+
+    // sum each element on same position to convert 55x55x3 acc into 55x55x1 output
+    for(int f=0;f<feat;f++){
+        for(int i=0;i<osize;i++){
+            for(int j=0;j<osize;j++){
+
+                output[f][i][j] = acc[0][i][j] + acc[1][i][j] + acc[2][i][j];
             }
         }
     }
@@ -45,16 +68,16 @@ void convolution(int*** input, float*** kernel, float*** output, int isize, int 
 
 
 /* ReLU non-linear function: apply the ReLU nonlinear function f(x)=max(0,x) to each input value
-   input: matrix on which applying ReLU function*/
-void relu(float*** relu, int size){
+   input: matrix on which applying ReLU function, size of matrix and number of features*/
+void relu(float*** relu, int size,int feat){
 
-    for(int k=0;k<3;k++){
+    for(int f=0;f<feat;f++){
 
         for(int i=0;i<size;i++){
 
             for(int j=0;j<size;j++){
 
-                relu[k][i][j]=max((float)0, relu[k][i][j]);
+                relu[f][i][j]=max((float)0, relu[f][i][j]);
             }
         }
     }
@@ -92,6 +115,8 @@ void maxpooling(int*** input, int*** output, int isize, int osize, int p, int st
 
 int main() {
 
+    // number of features
+    const int feat=1;
 
     // to pass matrices of different dimensions to a function we use a pointer of pointers
     // input images divided in 3 layers
@@ -121,27 +146,32 @@ int main() {
 
 
     // 3 kernels which will convolved on the 3 image matrices
-    float ***kernel;
+    float ****kernel;
     
-    kernel=new float **[3];
-    for(int i=0;i<11;i++){
-        kernel[i]=new float *[11];
+    kernel=new float ***[feat];
+    for(int i=0;i<3;i++){
+        kernel[i]=new float **[3];
 
         for(int j=0;j<11;j++){
+            kernel[i][j]=new float *[11];
 
-            kernel[i][j]=new float [11];
+            for(int k=0;k<11;k++){
+
+                kernel[i][j][k]= new float [11];
+            }
         }
     }
 
     
     // random values between -2 and 2 for each kernel
-    for(int i=0;i<3;i++){
+    
+    for(int f=0;f<feat;f++){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<11;j++){
+                for(int k=0;k<11;k++){
 
-        for(int j=0;j<11;j++){
-
-            for(int k=0;k<11;k++){
-
-                kernel[i][j][k]=rand()%5 - 2;
+                    kernel[f][i][j][k]=rand()%5 - 2;
+                }
             }
         }
     }
@@ -150,7 +180,7 @@ int main() {
     // output of convolutional operation between input and kernel of layer 1
     float ***layer1;
     
-    layer1=new float **[3];
+    layer1=new float **[feat];
     for(int i=0;i<55;i++){
         layer1[i]=new float *[55];
 
@@ -162,13 +192,11 @@ int main() {
 
     
     // inizialize convolutional matrix with only zero values
-    for(int i=0;i<3;i++){
-
+    for(int f=0;f<feat;f++){
         for(int j=0;j<55;j++){
-
             for(int k=0;k<55;k++){
 
-                layer1[i][j][k]=0;
+                layer1[f][j][k]=0;
             }
         }
     }
@@ -177,10 +205,10 @@ int main() {
 
     // LAYER 1
     // convolution between input image layers and kernels
-    convolution(input,kernel,layer1,224,11,55,1);
+    convolution(input,kernel,layer1,224,11,55,1,feat);
     
     // ReLU nonlinearity
-    relu(layer1,55);
+    relu(layer1,55,feat);
 
 
     // LAYER 2
