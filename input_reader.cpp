e2bbedee13638e5fa6490a51d5e0275a5d2143a2
Lorenzo <lorenzo.federico.porro@gmail.com>
2016-10-18 11:37:17 +0200

    input reader class completed

--- input_reader.cpp
+++ input_reader.cpp
@@ -1,41 +1,149 @@
-#include <stdio.h>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <map>
 
-unsigned char* readBMP(char* filename){
+using namespace std;
+//number of lines before the beginning of the pixel values list in the .txt file where pixel values are stored.
+const int FILE_OFFSET = 2;
 
-    int i,j;
-    FILE* f=fopen(filename,"rb");
+/*
+*Needed to check for (,), , characters in the pixel values .txt file.
+*
+*@param c: is the character to check
+*/
+bool IsChar(char c){
+	switch(c){
+	case '(':
+	case ')':
+	case ' ':
+		return true;
+	default:
+		return false;
+	}
+}
 
-    if(f==NULL) throw "Argument Exception";
+/*
+*Support method to use characters R,G,B to identify matrices.
+*
+*@param c: is the color identifier
+*/
+int RGB(char c){
+	switch(c){
+		case 'R':
+			return 1;
+		case 'G':
+			return 2;
+		case 'B':
+			return 3;
+		default:
+			return 0;	
+	}		
+}
 
-    unsigned char info[54];
-    fread(info,sizeof(unsigned char),54,f);
+/*
+*Reads the pixel values .txt file and return the desired pixel color value.
+*
+*@param s: is the line to read from the file
+*@param c: is the desired value (range: R, G, B)
+*/
+int split(const string &s, char c){
+	string str=s;
+	str.erase(remove_if(str.begin(),str.end(), &IsChar),str.end());
+	string delimiter=",";
+	string token;
+	int rgbValues[3];
+	size_t pos=0;
+	int i=0;
+	while ((pos=str.find(delimiter)) != string::npos){
+		token=str.substr(0,pos);
+		rgbValues[i]=stoi(token);
+		i++;
+		str.erase(0, pos+delimiter.length());	
+	}
+	rgbValues[2] = stoi(str);
+	return rgbValues[RGB(c)-1];
+}
 
-    int width=*(int*)&info[18];
-    int height=*(int*)&info[22];
+/*
+*Returns the width of the image (and of the matrix).
+*
+*@param filename: is the name of the pixel values .txt file
+*/
+int getWidth(string filename){
+	ifstream infile(filename);
+	int width;
+	int count=0;
+	string line;
+	while(getline(infile,line)) {
+		count++;
+		if (count==FILE_OFFSET-1){
+			width=stoi(line);
+			//cout << "width: "<< width << endl;
+			return width;
+		}
+	}
+}
 
-    int size=3*width*height;
-    unsigned char* data=new unsigned char[size];
-    fread(data,sizeof(unsigned char),size,f);
-    fclose(f);
+/*
+*Returns the height of the image (and of the matrix).
+*
+*@param filename: is the name of the pixel values .txt file
+*/
+int getHeight(string filename){
+	ifstream infile(filename);
+	int height;
+	int count=0;
+	string line;
+	while(getline(infile,line)) {
+		count++;
+		if (count==FILE_OFFSET){
+			height=stoi(line);
+			//cout << "height: "<< height << endl;
+			return height;
+		}
+	}
+}
 
-    unsigned char RedMatrix [224][224];
-    unsigned char GreenMatrix [224][224];
-    unsigned char BlueMatrix [224][224];
 
-    for(i=0;i<224;i++){
-        for(j=0;j<224;j++){
-            if(i>sizeOf(data)/sizeOf(unsigned char)||j>sizeOf(data)/sizeOf(unsigned char)){
-                RedMatrix[i][j]=0;
-                GreenMatrix[i][j]=0;
-                BlueMatrix[i][j]=0;
-            }
-            else{
-                RedMatrix[i][j]=data[j*width+i+2];
-                GreenMatrix[i][j]=data[j*width+i+1];
-                BlueMatrix[i][j]=data[j*width+i];
-            }
-        }
-    }
-    unsigned char pixels [3]={RedMatrix,GreenMatrix,BlueMatrix};
-    return pixels;
+
+map<char, vector < vector < int > >> getColors(){
+	string filename = "n00007846_6247.txt";
+	ifstream infile(filename);
+	string line;
+	const int width=getWidth(filename);
+	const int height=getHeight(filename);
+	int count=0;
+	map <char, vector < vector < int > > > colorMap;
+	vector< vector < int > > redMatrix(height, vector<int>(width));
+	vector< vector < int > > greenMatrix(height, vector<int>(width));
+	vector< vector < int > > blueMatrix(height, vector<int>(width));
+	int position=1;
+	int i=0;
+	int j=0;
+	float rest=0;
+	while(getline(infile,line)){
+		if(position<=FILE_OFFSET){
+			position++;			
+			continue;
+		}
+		redMatrix[i][j]=split(line, 'R');
+		greenMatrix[i][j]=split(line, 'G');
+		blueMatrix[i][j]=split(line, 'B');
+		//cout << "i: " << i << ", j: " << j << ", R: " << redMatrix[i][j] << ", G: " << greenMatrix[i][j] << ", B: " << blueMatrix[i][j] << endl;
+		rest = (position-FILE_OFFSET) % width;
+		if(rest==0){
+			j=0;		
+			i++;
+		}else	j++;
+		position++;  	
+	}
+	colorMap['R']=redMatrix;
+	colorMap['G']=greenMatrix;
+	colorMap['B']=blueMatrix;
+	//cout << "Done! Map size: " << colorMap.size() << endl;
+    return colorMap;
 }
