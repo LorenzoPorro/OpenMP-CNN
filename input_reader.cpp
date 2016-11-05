3593d7aa19fe40acaaa7c079247284dedf5723a2
Lorenzo <lorenzo.federico.porro@gmail.com>
2016-11-05 18:31:58 +0100

    a

--- input_reader.cpp
+++ input_reader.cpp
@@ -5,145 +5,113 @@
 #include <algorithm>
 #include <vector>
 #include <map>
+#include <regex>
+
 
 using namespace std;
-//number of lines before the beginning of the pixel values list in the .txt file where pixel values are stored.
-const int FILE_OFFSET = 2;
 
 /*
-*Needed to check for (,), , characters in the pixel values .txt file.
-*
-*@param c: is the character to check
-*/
-bool IsChar(char c){
-	switch(c){
-	case '(':
-	case ')':
-	case ' ':
-		return true;
-	default:
-		return false;
-	}
-}
-
-/*
-*Support method to use characters R,G,B to identify matrices.
-*
-*@param c: is the color identifier
-*/
-int RGB(char c){
-	switch(c){
-		case 'R':
-			return 1;
-		case 'G':
-			return 2;
-		case 'B':
-			return 3;
-		default:
-			return 0;	
-	}		
-}
-
-/*
-*Reads the pixel values .txt file and return the desired pixel color value.
+*Reads the pixel values .txt file and returns an array of color values for the specific line.
 *
 *@param s: is the line to read from the file
 *@param c: is the desired value (range: R, G, B)
 */
-int split(const string &s, char c){
-	string str=s;
-	str.erase(remove_if(str.begin(),str.end(), &IsChar),str.end());
-	string delimiter=",";
-	string token;
-	int rgbValues[3];
-	size_t pos=0;
+vector<int> split(string s, char c){
+	vector<int> values(224);
 	int i=0;
-	while ((pos=str.find(delimiter)) != string::npos){
-		token=str.substr(0,pos);
-		rgbValues[i]=stoi(token);
-		i++;
-		str.erase(0, pos+delimiter.length());	
+	switch(c){
+		case 'R':
+		{
+			regex rx ("(\\()([0-9]+)");
+			string str;
+			regex_iterator<string::iterator> rit(s.begin(),s.end(), rx);
+			regex_iterator<string::iterator> rend;
+			while(rit !=rend){
+				str=rit->str();
+				str.erase(remove(str.begin(), str.end(), '('), str.end());
+				values[i]=stoi(str);
+				++rit;
+				i++;
+			}
+		}
+		case 'G':
+		{
+			regex rx ("(\\,\\.)([0-9]+)(\\,)");
+			string str;
+			regex_iterator<string::iterator> rit(s.begin(),s.end(), rx);
+			regex_iterator<string::iterator> rend;
+			while(rit !=rend){
+				str=rit->str();
+				str.erase(remove(str.begin(), str.end(), ','), str.end());
+				values[i]=stoi(str);
+				++rit;
+				i++;
+			}
+		}
+		case 'B':
+		{
+			regex rx ("([0-9]+)(\\()");
+			string str;
+			regex_iterator<string::iterator> rit(s.begin(),s.end(), rx);
+			regex_iterator<string::iterator> rend;
+			while(rit !=rend){
+				str=rit->str();
+				str.erase(remove(str.begin(), str.end(), ')'), str.end());
+				values[i]=stoi(str);
+				++rit;
+				i++;
+			}
+		}
+		default:
+			break;
 	}
-	rgbValues[2] = stoi(str);
-	return rgbValues[RGB(c)-1];
+	return values;
 }
 
 /*
-*Returns the width of the image (and of the matrix).
+*Returns a map (R,G,B) with the matrices of pixel color values
 *
-*@param filename: is the name of the pixel values .txt file
 */
-int getWidth(string filename){
-	ifstream infile(filename);
-	int width;
-	int count=0;
-	string line;
-	while(getline(infile,line)) {
-		count++;
-		if (count==FILE_OFFSET-1){
-			width=stoi(line);
-			//cout << "width: "<< width << endl;
-			return width;
-		}
-	}
-}
-
-/*
-*Returns the height of the image (and of the matrix).
-*
-*@param filename: is the name of the pixel values .txt file
-*/
-int getHeight(string filename){
-	ifstream infile(filename);
-	int height;
-	int count=0;
-	string line;
-	while(getline(infile,line)) {
-		count++;
-		if (count==FILE_OFFSET){
-			height=stoi(line);
-			//cout << "height: "<< height << endl;
-			return height;
-		}
-	}
-}
-
-
-
-map<char, vector < vector < int > >> getColors(){
-	string filename = "n00007846_6247.txt";
+map<char, vector < vector < int > >> getColors(string filename){
 	ifstream infile(filename);
 	string line;
-	const int width=getWidth(filename);
-	const int height=getHeight(filename);
-	int count=0;
+	const int width=224;
+	const int height=224;
 	map <char, vector < vector < int > > > colorMap;
 	vector< vector < int > > redMatrix(height, vector<int>(width));
 	vector< vector < int > > greenMatrix(height, vector<int>(width));
 	vector< vector < int > > blueMatrix(height, vector<int>(width));
 	int position=1;
-	int i=0;
-	int j=0;
-	float rest=0;
+	int row=0;
 	while(getline(infile,line)){
-		if(position<=FILE_OFFSET){
-			position++;			
-			continue;
-		}
-		redMatrix[i][j]=split(line, 'R');
-		greenMatrix[i][j]=split(line, 'G');
-		blueMatrix[i][j]=split(line, 'B');
-		//cout << "i: " << i << ", j: " << j << ", R: " << redMatrix[i][j] << ", G: " << greenMatrix[i][j] << ", B: " << blueMatrix[i][j] << endl;
-		rest = (position-FILE_OFFSET) % width;
-		if(rest==0){
-			j=0;		
-			i++;
-		}else	j++;
-		position++;  	
+		cout << "line "<< row << endl;
+		redMatrix[row]=split(line,'R');
+		greenMatrix[row]=split(line,'G');
+		blueMatrix[row]=split(line,'B');
+		row++; 	
 	}
 	colorMap['R']=redMatrix;
 	colorMap['G']=greenMatrix;
 	colorMap['B']=blueMatrix;
-	//cout << "Done! Map size: " << colorMap.size() << endl;
-    return colorMap;
+	return colorMap;
 }
+
+vector<map<char,vector<vector<int>>>> getInputArray(){
+	vector<map<char,vector<vector<int>>>> array;
+	string filename="file_list.txt";
+	ifstream infile(filename);
+	string line;
+	int i=0;
+	while(getline(infile,line)){
+		cout << line << endl;
+		array.push_back(getColors(line));
+		i++;
+		cout << "Element" << line <<  "done (" << i << ")" << endl;	
+	}
+}
+
+int main(){
+	cout << getInputArray().size() << endl;
+}
+
+
